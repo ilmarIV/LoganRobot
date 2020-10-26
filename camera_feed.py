a19a44d34d54e836fc7bfbe7cf40bde06f47c122
ilmarIV <72752986+ilmarIV@users.noreply.github.com>
2020-10-26 18:08:52 +0200

    Update camera_feed.py

--- camera_feed.py
+++ camera_feed.py
@@ -18,19 +18,18 @@ try:
         depth_frame = frames.get_depth_frame()
         color_frame = frames.get_color_frame()
 
-        #if(depth_frame and color_frame):
-        depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
+        if(depth_frame and color_frame):
+            depth_image = np.asanyarray(depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
 
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-        images = np.hstack((color_image, depth_colormap))
+        #depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        #images = np.hstack((color_image, depth_colormap))
 
         cv2.imshow("RealSense", color_image)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         
-    cap.release()
     cv2.destroyAllWindows() 
 
 finally:
