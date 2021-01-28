84be48a4f08fee62998ed1370dadc87268872bbc
ilmarIV <72752986+ilmarIV@users.noreply.github.com>
2021-01-28 19:27:57 +0200

    Delete project directory

--- project/Mainboard.py
+++
@@ -1,9 +0,0 @@
-from serial import Serial
-
-ser = Serial('/dev/ttyACM0', baudrate=115200, timeout=1)
-
-def mainboardMSG(speed_1, speed_2, speed_3):
-    msg = "sd:" + str(speed_1) + ":" + str(speed_2) + ":" + str(speed_3) + "\r\n"
-    ser.write(msg.encode('UTF-8'))
-    while ser.inWaiting() > 0:
-        ser.read()
